5f60a3e37b4f53c719d8dbe97fb152230c765154
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-12-04 01:25:12 +0500

    angular material is configured.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslocoRootModule } from './transloco-root.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslocoRootModule],
+  imports: [RouterOutlet, TranslocoRootModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
